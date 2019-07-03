5fd6c50e2921d9ed41f0cbdc1078ad4f1847a7e8
nicolinocuralli <nicolino.curalli@gmail.com>
2019-07-03 12:39:31 +0200

    added chef  bash connfiguration dependences

---
+++ bashrc.d/chef.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Title
+#          Description
+# ------------------------------------------------------------------
+
+eval "$(chef shell-init bash)"
+export PATH="/opt/chefdk/embedded/bin:$PATH"
